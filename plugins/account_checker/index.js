57ab9a3462fd078d3e2d7fb6f35338617e2f7a1e
balala2oo8 <xiongxin2oo8@vip.qq.com>
2018-11-11 12:44:09 +0800

    bug fix

--- plugins/account_checker/index.js
+++ plugins/account_checker/index.js
@@ -375,7 +375,7 @@ const checkAccount = async (serverId, accountId) => {
         for (const account of accounts) {
           const start = Date.now();
           //console.log('checkAccount1', account.serverId, account.accountId);
-          await checkAccount(account.serverId, account.accountId).catch();
+          await checkAccount('378',account, account.accountId).catch();
           const time = 60 * 1000 / accounts.length - (Date.now() - start);
           await sleep((time <= 0 || time > sleepTime) ? sleepTime : time);
         }
