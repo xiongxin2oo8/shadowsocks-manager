b118a3a262afcb48aef0968d8606dbf8c4de06ab
balala <xiongxin2oo8@vip.qq.com>
2018-12-28 11:03:35 +0800

    添加允许更换套餐字段

--- plugins/webgui_order/db/webgui_order.js
+++ plugins/webgui_order/db/webgui_order.js
@@ -3,7 +3,7 @@ const tableName = 'webgui_order';
 
 const addDefaultOrder = async () => {
   const data = await knex('webgui_order').where({}).then(s => s[0]);
-  if(!data) {
+  if (!data) {
     const oldData = await knex('webguiSetting').where({ key: 'payment' }).then(s => JSON.parse(s[0].value));
     const types = {
       2: 'week',
@@ -14,10 +14,10 @@ const addDefaultOrder = async () => {
       7: 'year',
     };
     const insertData = [];
-    for(const type in types) {
+    for (const type in types) {
       let cycle = 1;
-      if(+type === 6) { cycle = 3; }
-      if(+type === 7) { cycle = 12; }
+      if (+type === 6) { cycle = 3; }
+      if (+type === 7) { cycle = 12; }
       insertData.push({
         id: type,
         name: oldData[types[type]].orderName || types[type],
@@ -39,49 +39,56 @@ const addDefaultOrder = async () => {
 };
 
 const fixRefTime = async () => {
-  await knex.schema.alterTable(tableName, function(table) {
+  await knex.schema.alterTable(tableName, function (table) {
     table.bigInteger('refTime').alter();
   });
 };
 
 const createTable = async () => {
   const exist = await knex.schema.hasTable(tableName);
-  if(exist) {
+  if (exist) {
+
     const hasAutoRemoveDelay = await knex.schema.hasColumn(tableName, 'autoRemoveDelay');
-    if(!hasAutoRemoveDelay) {
-      await knex.schema.table(tableName, function(table) {
+    if (!hasAutoRemoveDelay) {
+      await knex.schema.table(tableName, function (table) {
         table.bigInteger('autoRemoveDelay').defaultTo(0);
       });
     }
     const hasPortRange = await knex.schema.hasColumn(tableName, 'portRange');
-    if(!hasPortRange) {
-      await knex.schema.table(tableName, function(table) {
+    if (!hasPortRange) {
+      await knex.schema.table(tableName, function (table) {
         table.string('portRange').defaultTo('0');
       });
     }
     const hasShortComment = await knex.schema.hasColumn(tableName, 'shortComment');
-    if(!hasShortComment) {
-      await knex.schema.table(tableName, function(table) {
+    if (!hasShortComment) {
+      await knex.schema.table(tableName, function (table) {
         table.string('shortComment').defaultTo('');
       });
     }
     const hasBaseId = await knex.schema.hasColumn(tableName, 'baseId');
-    if(!hasBaseId) {
-      await knex.schema.table(tableName, function(table) {
+    if (!hasBaseId) {
+      await knex.schema.table(tableName, function (table) {
         table.integer('baseId').defaultTo(0);
       });
     }
     const hasActive = await knex.schema.hasColumn(tableName, 'active');
-    if(!hasActive) {
-      await knex.schema.table(tableName, function(table) {
+    if (!hasActive) {
+      await knex.schema.table(tableName, function (table) {
         table.integer('active').defaultTo(1);
+      });
+    }
+    const hasChangeOrderType= await knex.schema.hasColumn(tableName, 'changeOrderType');
+    if (!hasChangeOrderType) {
+      await knex.schema.table(tableName, function (table) {
+        table.integer('changeOrderType').defaultTo(0);
       });
     }
     await addDefaultOrder();
     await fixRefTime();
     return;
   }
-  await knex.schema.createTable(tableName, function(table) {
+  await knex.schema.createTable(tableName, function (table) {
     table.increments('id').primary();
     table.integer('baseId').defaultTo(0);
     table.string('name');
