b88ba04b9aa22784140f8946d54c648ea86da9e3
balala <xiongxin2oo8@vip.qq.com>
2019-01-28 12:19:58 +0800

    bug fix

--- init/runShadowsocks.js
+++ init/runShadowsocks.js
@@ -9,22 +9,23 @@ const run = async () => {
   let runParams = config.runShadowsocks;
   let type = 'libev';
   let method = 'aes-256-cfb';
-  if(!runParams) {
+  if (!runParams) {
     return;
   }
-  if(typeof runParams === 'boolean' && runParams) {
+  if (typeof runParams === 'boolean' && runParams) {
     runParams = '';
   }
-  if(runParams.indexOf(':') >= 0) {
+  if (runParams.indexOf(':') >= 0) {
     method = runParams.split(':')[1];
   }
   let shadowsocks;
-  if(runParams.indexOf('python') >= 0) {
+  if (runParams.indexOf('python') >= 0) {
     type = 'python';
+    const port = config.shadowsocks.initport || 65535;
     const tempPassword = 'qwerASDF' + Math.random().toString().substr(2, 8);
-    shadowsocks = spawn('ssserver', ['-m', method, '-p', '65535', '-k', tempPassword, '--manager-address', config.shadowsocks.address]);
+    shadowsocks = spawn('ssserver', ['-m', method, '-p', port, '-k', tempPassword, '--manager-address', config.shadowsocks.address]);
   } else {
-    shadowsocks = spawn('ss-manager', [ '-v', '-m', method, '-u', '--manager-address', config.shadowsocks.address]);
+    shadowsocks = spawn('ss-manager', ['-v', '-m', method, '-u', '--manager-address', config.shadowsocks.address]);
   }
 
   shadowsocks.stdout.on('data', (data) => {
@@ -38,7 +39,7 @@ const run = async () => {
   shadowsocks.on('close', (code) => {
     logger.error(`child process exited with code ${code}`);
   });
-  logger.info(`Run shadowsocks (${ type === 'python' ? 'python' : 'libev'})`);
+  logger.info(`Run shadowsocks (${type === 'python' ? 'python' : 'libev'})`);
   return;
 };
 
