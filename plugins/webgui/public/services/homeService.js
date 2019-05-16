696fecd20fd74e3d5e74fc1cd05c223fd54336ac
balala <xiongxin2oo8@vip.qq.com>
2019-05-16 13:51:51 +0800

    update

--- plugins/webgui/public/services/homeService.js
+++ plugins/webgui/public/services/homeService.js
@@ -49,7 +49,8 @@ app.factory('homeApi', ['$http', $http => {
         if(err.data === 'invalid ref code') { errData = '发送错误，无效的邀请码'; }
         if(err.data === 'email in black list') { errData = '发送错误，请更换邮箱尝试'; }
         if(err.data === 'send email out of limit') { errData = '请求过于频繁，请稍后再试'; }
-        if(err.data === 'signup close') { errData = '当前时段尚未开放注册'; }
+        if(err.data === 'signup close') { errData = '已停止服务'; }
+        //if(err.data === 'signup close') { errData = '当前时段尚未开放注册'; }
         return Promise.reject(errData);
       } else {
         return Promise.reject('网络异常，请稍后再试');
